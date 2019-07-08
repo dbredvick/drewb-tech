479a196dc66ea9d856e67fa2cd3b82e8921cc169
Drew Bredvick <drew.bredvick@spreetail.com>
2019-07-07 23:50:01 -0500

    fixing links for now

--- src/utils/link.js
+++ src/utils/link.js
@@ -11,18 +11,18 @@ const Link = ({ children, to, activeClassName, partiallyActive, ...other }) => {
     const internal = /^\/(?!\/)/.test(to);
 
     // Use Gatsby Link for internal links, and <a> for others
-    if (internal) {
-        return (
-            <GatsbyLink
-                to={to}
-                activeClassName={activeClassName}
-                partiallyActive={partiallyActive}
-                {...other}
-            >
-                {children}
-            </GatsbyLink>
-        )
-    }
+    // if (internal) {
+    //     return (
+    //         <GatsbyLink
+    //             to={to}
+    //             activeClassName={activeClassName}
+    //             partiallyActive={partiallyActive}
+    //             {...other}
+    //         >
+    //             {children}
+    //         </GatsbyLink>
+    //     )
+    // }
     return (
         <a href={to} {...other}>
             {children}
