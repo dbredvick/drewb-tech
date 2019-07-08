0cbe0bee736c0edf7168d50c393641993469d428
Drew Bredvick <drew.bredvick@spreetail.com>
2019-07-07 23:39:48 -0500

    trying to fix mobile links

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -1,29 +1,32 @@
-window.onGatsbyInitialClientRender = function() {
-/**
- * Main JS file for theme behaviours
- */
+window.onGatsbyInitialClientRender = function () {
+  /**
+   * Main JS file for theme behaviours
+   */
 
-// Responsive video embeds
-var videoEmbeds = [
-  'iframe[src*="youtube.com"]',
-  'iframe[src*="vimeo.com"]'
-];
-reframe(videoEmbeds.join(','));
+  // Responsive video embeds
+  var videoEmbeds = [
+    'iframe[src*="youtube.com"]',
+    'iframe[src*="vimeo.com"]'
+  ];
+  reframe(videoEmbeds.join(','));
 
-// Mobile menu
-var menuToggle = document.querySelectorAll('.menu-toggle');
-if (menuToggle) {
-  for (var i = 0; i < menuToggle.length; i++) {
-    menuToggle[i].addEventListener('click', function(e){
-      document.body.classList.toggle('menu--opened');
-      e.preventDefault();
-    },false);
-  }
-  window.addEventListener('resize', function () {
-    if (menuToggle.offsetParent === null) {
-      document.body.classList.remove('menu--opened');
+  // Mobile menu
+  var menuToggle = document.querySelectorAll('.menu-toggle');
+  if (menuToggle) {
+    for (var i = 0; i < menuToggle.length; i++) {
+      menuToggle[i].addEventListener('click', function (e) {
+        document.body.classList.toggle('menu--opened');
+        e.preventDefault();
+      }, false);
+      menuToggle[i].addEventListener('touchend', function (e) {
+        document.body.classList.toggle('menu--opened');
+        e.preventDefault();
+      }, false);
     }
-  }, true);
-}
-
+    window.addEventListener('resize', function () {
+      if (menuToggle.offsetParent === null) {
+        document.body.classList.remove('menu--opened');
+      }
+    }, true);
+  }
 };
